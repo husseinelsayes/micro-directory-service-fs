b82c398e997182d0417f6af678eea8fab5318df2
Hussein Emam <eng.hussein.elsayes@gmail.com>
2020-09-12 01:54:21 +0300

    f

--- src/web/webapp/src/app/app.component.ts
+++ src/web/webapp/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { OAuthService} from 'angular-oauth2-oidc';
 import { authConfig } from './services/sso.config';
 import { JwksValidationHandler } from 'angular-oauth2-oidc-jwks';
+import { NavigationService } from './services/navigation.service';
 
 
 @Component({
@@ -10,14 +11,26 @@ import { JwksValidationHandler } from 'angular-oauth2-oidc-jwks';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  constructor(private router: Router,private _oauthService : OAuthService) {
+export class AppComponent implements OnInit , OnChanges{
+  @Input('state')
+  set state(state: string) {
+    //console.debug('client received state', state);
+  }
+  
+  constructor(private router: Router,private _oauthService : OAuthService,public navService: NavigationService) {
     //this.configureSSo();
+  }
+
+
+  ngOnChanges(changes: SimpleChanges) {
+    //console.log('current state value ',changes.state.currentValue);
+    this.navService.sidebarState.sidenavOpen = JSON.parse(changes.state.currentValue).sidenavOpen;
+    this.navService.sidebarState.childnavOpen = JSON.parse(changes.state.currentValue).childnavOpen;
   }
   
   ngOnInit() {
     this.router.initialNavigation();
+    //this.toggelSidebar();
   }
 
   configureSSo(){
@@ -26,6 +39,23 @@ export class AppComponent implements OnInit {
     this._oauthService.tryLogin();
     if(!this._oauthService.hasValidAccessToken()){
       this._oauthService.initImplicitFlow();
+    }
+  }
+
+
+  toggelSidebar() {
+    const state = this.navService.sidebarState;
+    if (state.childnavOpen && state.sidenavOpen) {
+      return state.childnavOpen = false;
+    }
+    if (!state.childnavOpen && state.sidenavOpen) {
+      return state.sidenavOpen = false;
+    }
+    if (!state.sidenavOpen && !state.childnavOpen) {
+        state.sidenavOpen = true;
+        setTimeout(() => {
+            state.childnavOpen = true;
+        }, 50);
     }
   }
 }
